79e3c26f899ff97a6788435d9633fdd92b11e15e
Derek Zhan <derekjan1240@gmail.com>
2021-03-11 12:32:33 +0800

    完成 Dog CRUD

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,6 +1,5 @@
 import {
   Controller,
-  UsePipes,
   Get,
   Post,
   Body,
@@ -9,37 +8,47 @@ import {
   Delete,
 } from '@nestjs/common';
 import { DogsService } from './dogs.service';
+import { DogDto } from './dto/dog.dto';
 import { CreateDogDto } from './dto/create-dog.dto';
 import { UpdateDogDto } from './dto/update-dog.dto';
-import { ValidationPipe } from '../pipe/validation.pipe';
+import { User } from '../common/decorators/user.decorator';
+import { User as UserEntity } from 'src/database/entities/user.entity';
 
 @Controller('dogs')
 export class DogsController {
   constructor(private readonly dogsService: DogsService) {}
 
-  @UsePipes(new ValidationPipe())
   @Post()
-  create(@Body() body: CreateDogDto) {
-    return this.dogsService.create(body);
+  public async create(
+    @Body() dto: CreateDogDto,
+    @User() user: UserEntity,
+  ): Promise<CreateDogDto> {
+    const dog = CreateDogDto.from(dto);
+    return this.dogsService.create(dog, user);
   }
 
   @Get()
-  findAll() {
-    return this.dogsService.findAll();
+  public async findAll(): Promise<DogDto[]> {
+    return await this.dogsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.dogsService.findOne(+id);
+  public async findOne(@Param('id') id: string): Promise<DogDto> {
+    return await this.dogsService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateDogDto: UpdateDogDto) {
-    return this.dogsService.update(+id, updateDogDto);
+  public async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateDogDto,
+    @User() user: UserEntity,
+  ): Promise<UpdateDogDto> {
+    const dog = UpdateDogDto.from(dto);
+    return this.dogsService.update(id, dog, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dogsService.remove(+id);
+  public async remove(@Param('id') id: string): Promise<DogDto> {
+    return await this.dogsService.remove(id);
   }
 }
--- src/dogs/dogs.service.ts
+++ src/dogs/dogs.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Dog } from 'src/database/entities/dog.entity';
+import { User } from 'src/database/entities/user.entity';
+import { DogDto } from './dto/dog.dto';
 import { CreateDogDto } from './dto/create-dog.dto';
 import { UpdateDogDto } from './dto/update-dog.dto';
 
 @Injectable()
 export class DogsService {
-  create(createDogDto: CreateDogDto) {
-    return 'This action adds a new dog';
+  constructor(
+    @InjectRepository(Dog)
+    private readonly dogRepository: Repository<Dog>,
+  ) {}
+
+  public async create(dto: CreateDogDto, user: User): Promise<CreateDogDto> {
+    return this.dogRepository
+      .save(dto.toEntity(user))
+      .then((e) => CreateDogDto.fromEntity(e));
   }
 
-  findAll() {
-    return `This action returns all dogs`;
+  public async findAll(): Promise<DogDto[]> {
+    return await this.dogRepository
+      .find()
+      .then((dogs) => dogs.map((e) => DogDto.fromEntity(e)));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dog`;
+  public async findOne(id: string): Promise<DogDto> {
+    const dog = await this.dogRepository.findOne(id);
+    if (!dog) {
+      throw new Error(`The dog with id: ${id} does not exist!`);
+    }
+    return DogDto.fromEntity(dog);
   }
 
-  update(id: number, updateDogDto: UpdateDogDto) {
-    return `This action updates a #${id} dog`;
+  public async update(
+    id: string,
+    dto: UpdateDogDto,
+    user: User,
+  ): Promise<UpdateDogDto> {
+    const dog = await this.dogRepository.findOne(id);
+    if (!dog) {
+      throw new Error(`The dog with id: ${id} does not exist!`);
+    }
+    Object.assign(dog, dto.toEntity(user));
+    return this.dogRepository.save(dog).then((e) => UpdateDogDto.fromEntity(e));
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} dog`;
+  public async remove(id: string): Promise<DogDto> {
+    const dog = await this.dogRepository.findOne(id);
+    if (!dog) {
+      throw new Error(`The dog with id: ${id} does not exist!`);
+    }
+    return this.dogRepository.remove(dog).then((e) => DogDto.fromEntity(e));
   }
 }
